4da54bad9b6fb3201e3904f8a80067774fa81cb6
朱云龙 <zhu_yunlong@czcg.com.cn>
2017-09-11 14:42:10 +0800

    添加用户是否可以选择原图

--- Sources/ImagePicker/YLPhotoPickerController.swift
+++ Sources/ImagePicker/YLPhotoPickerController.swift
@@ -35,6 +35,7 @@ class YLPhotoPickerController: UIViewController {
         let toolbar = YLToolbarBottom.loadNib()
         toolbar.sendBtnIsSelect(false)
         toolbar.sendBtn.addTarget(self, action: #selector(YLPhotoPickerController.sendBtnHandle), for: UIControlEvents.touchUpInside)
+        toolbar.originalImageClickBtn.addTarget(self, action: #selector(YLPhotoPickerController.originalImageClickBtnHandle), for: UIControlEvents.touchUpInside)
         return toolbar
     }()
     
@@ -97,6 +98,8 @@ class YLPhotoPickerController: UIViewController {
         
         if imagePicker.isOneChoose == false {
             
+            toolbar.originalImageBtnIsSelect(imagePicker.isSelectedOriginalImage)
+            
             view.addSubview(toolbar)
             // 约束
             toolbar.translatesAutoresizingMaskIntoConstraints = false
@@ -150,8 +153,35 @@ class YLPhotoPickerController: UIViewController {
         
     }
     
+    /// 发送按钮
     func sendBtnHandle() {
         epPhotoBrowserBySendBtnHandle(-1)
+    }
+    
+    /// 选择原图
+    func originalImageClickBtnHandle() {
+        
+        let imagePicker = self.navigationController as! YLImagePickerController
+        let isSelectedOriginalImage = imagePicker.isSelectedOriginalImage
+        toolbar.originalImageBtnIsSelect(!isSelectedOriginalImage)
+        imagePicker.isSelectedOriginalImage = !isSelectedOriginalImage
+        
+    }
+    
+    /// 获取用户导出图片大小
+    func getUserNeedSize(_ size: CGSize) -> CGSize {
+        var width:CGFloat = CGFloat(size.width)
+        var height:CGFloat = CGFloat(size.height)
+        // 是否选择了原图
+        let imagePicker = navigationController as! YLImagePickerController
+        if imagePicker.isSelectedOriginalImage == false &&
+            width > imagePicker.photoWidth {
+            
+            width = imagePicker.photoWidth
+            height = CGFloat(size.height) / CGFloat(size.width) * width
+            
+        }
+        return CGSize.init(width: width, height: height)
     }
 }
 
@@ -267,7 +297,7 @@ extension YLPhotoPickerController :YLThumbnailCellDelegate {
         
         if imagePicker.isOneChoose == true {
             
-            PHImageManager.default().requestImage(for: assetModel.asset, targetSize: CGSize.init(width: assetModel.asset.pixelWidth, height: assetModel.asset.pixelHeight), contentMode: PHImageContentMode.aspectFill, options: options, resultHandler: { (result:UIImage?, _) in
+            PHImageManager.default().requestImage(for: assetModel.asset, targetSize: getUserNeedSize(CGSize.init(width: assetModel.asset.pixelWidth, height: assetModel.asset.pixelHeight)), contentMode: PHImageContentMode.aspectFill, options: options, resultHandler: { (result:UIImage?, _) in
                 
                 if let image = result {
                     
@@ -388,7 +418,7 @@ extension YLPhotoPickerController: YLPhotoBrowserDelegate {
         var images = [UIImage]()
         for assetModel in selectPhotos {
             
-            PHImageManager.default().requestImage(for: assetModel.asset, targetSize: CGSize.init(width: assetModel.asset.pixelWidth, height: assetModel.asset.pixelHeight), contentMode: PHImageContentMode.aspectFill, options: options, resultHandler: { (result:UIImage?, _) in
+            PHImageManager.default().requestImage(for: assetModel.asset, targetSize: getUserNeedSize(CGSize.init(width: assetModel.asset.pixelWidth, height: assetModel.asset.pixelHeight)), contentMode: PHImageContentMode.aspectFill, options: options, resultHandler: { (result:UIImage?, _) in
                 
                 if let image = result {
                     images.append(image)
@@ -398,7 +428,6 @@ extension YLPhotoPickerController: YLPhotoBrowserDelegate {
         
         let imagePicker = navigationController as! YLImagePickerController
         imagePicker.didFinishPickingPhotosHandle?(images)
-        
         imagePicker.goBack()
         
     }
